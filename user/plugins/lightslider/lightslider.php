6bd8e6ef0e3814578fa2250c63d6049ab68a34fd
Samuel <mail@samuelstein.de>
2015-06-20 15:11:00 +0200

    adding lightslider plugin

---
+++ user/plugins/lightslider/lightslider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Grav\Plugin;
+
+use \Grav\Common\Plugin;
+use \Grav\Common\Grav;
+use \Grav\Common\Page\Page;
+
+class LightsliderPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize configuration
+     */
+    public function onPluginsInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    /**
+     * Set needed variables to display cart.
+     */
+    public function onTwigSiteVariables()
+    {
+        if ($this->config->get('plugins.lightslider.built_in_css')) {
+            $this->grav['assets']
+                ->add('plugin://lightslider/css/lightslider-core.css')
+                ->add('plugin://lightslider/css/lightslider-custom.css')
+                ->add('plugin://lightslider/js/jquery.lightSlider.min.js');
+        }
+    }
+}
