9b0a0410b94fef478f42a3dab16e7edd71fb6d54
samuelstein <mail@samuelstein.de>
2016-05-19 17:49:23 +0200

    updating plugins

--- cache/compiled/files/0309c8f26f845016d2a3fb91083aab51.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/samuelstein/WORK/foggade/website/user/config/security.yaml',
-    'modified' => 1449080555,
-    'data' => [
-        'salt' => 'Wz7fAbZGSnC18B'
-    ]
-];
